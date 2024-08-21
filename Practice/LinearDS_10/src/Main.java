6e9dfa7d787baf2fe0dfd259faaa905e253f80c9
heeejinin <150228975+heeejinin@users.noreply.github.com>
2024-08-21 18:55:14 +0900

    선형 자료구조 - 큐, 데크
    큐 연습문제 코드
    데크 구현 코드, 연습문제 코드

---
+++ Practice/LinearDS_10/src/Main.java
@@ -0,0 +1,40 @@
+// 선형 자료구조 - 데크
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque deque = new ArrayDeque();
+
+        // Front 부분 입력
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        System.out.println(deque); // [3, 2, 1]
+
+        // Rear 부분 입력
+        deque.addLast(10);
+        deque.addLast(20);
+        deque.addLast(30);
+        System.out.println(deque); // [3, 2, 1, 10, 20, 30]
+
+        // Front 부분 출력
+        System.out.println(deque.removeFirst()); // 3
+        System.out.println(deque); // [2, 1, 10, 20, 30]
+
+        // Rear 부분 출력
+        System.out.println(deque.removeLast()); // 30
+        System.out.println(deque); // [2, 1, 10, 20]
+
+        System.out.println(deque.removeLast()); // 20
+        System.out.println(deque.removeLast()); // 10
+        System.out.println(deque.removeLast()); // 1
+        System.out.println(deque.removeLast()); // 2
+        System.out.println(deque); // []
+
+        System.out.println(deque.pollLast()); // null
+        System.out.println(deque.removeLast()); // 예외 발생
+    }
+}
