c0d6de98ac4c653f89640dbcc64e84db1bf6ffd3
heeejinin <150228975+heeejinin@users.noreply.github.com>
2024-09-01 20:59:14 +0900

    그리디 알고리즘
    그리디 알고리즘  구현 및 연습 문제 코드

---
+++ Algorithm/Algorithm_09/src/Practice5.java
@@ -0,0 +1,50 @@
+// Practice
+// 정수 num 이 주어졌을 때,
+// num 숫자에서 두 자리를 최대 한번만 교체하여 얻을 수 있는 최대값을 출력하세요.
+
+// 입출력 예시
+// num: 2736
+// 출력: 7236
+
+// 입력: 7116
+// 출력: 7611
+
+public class Practice5 {
+    public static int solution(int num) {
+        char[] cArr = String.valueOf(num).toCharArray();
+        int[] maxArr = new int[cArr.length];
+
+        int max = 0;
+        for (int i = cArr.length - 1; i >= 0; i--) {
+            // char - '0' => '0' 문자를 기준으로 다른 숫자 문자를 정수로 변환
+            max = Math.max(max, cArr[i] - '0');
+            maxArr[i] = max;
+        }
+
+        for (int i = 0; i < cArr.length - 1; i++) {
+            if (cArr[i] - '0' < maxArr[i + 1]) {
+                // maxArr[i + 1]의 인덱스가 어딘지 찾기
+                for (int j = cArr.length - 1; j >= i + 1; j--) {
+                    if (cArr[j] - '0' == maxArr[i + 1]) {
+                        char tmp = cArr[j];
+                        cArr[j] = cArr[i];
+                        cArr[i] = tmp;
+
+                        //char 배열 Int로 변환하여 반환
+                        return Integer.parseInt(String.valueOf(cArr));
+                    }
+                }
+            }
+        }
+
+
+        return num;
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        System.out.println(solution(2736)); // 7263
+        System.out.println(solution(7116)); // 7611
+        System.out.println(solution(91)); // 91
+    }
+}
